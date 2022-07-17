7fc1f8f92656ba12bd9a3b9c2abbeaa3dfcb8e53
Lucien Shaw <myxlc55@outlook.com>
2022-07-17 14:31:01 +0800

    fix: remove a dependency (socket)

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     long_description=long_description,
     packages=setuptools.find_packages(),
-    install_requires=['socket','selenium'],
+    install_requires=['selenium'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
