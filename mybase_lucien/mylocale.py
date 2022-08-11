1c80a6c1cb6a880230a412ca43de8945c5b60539
Lucien Shaw <myxlc55@outlook.com>
2022-08-11 10:29:45 +0800

    fix: get correct path when imported

--- mybase_lucien/mylocale.py
+++ mybase_lucien/mylocale.py
@@ -4,11 +4,11 @@ def is_exec():
         return True
     else:
         return False
-def get_dir():
+def get_dir(myfile=__file__):
     import os
     if is_exec():
         import sys
         mydir=os.path.dirname(os.path.abspath(sys.executable))
     else:
-        mydir=os.path.dirname(os.path.abspath(__file__))
+        mydir=os.path.dirname(os.path.abspath(myfile))
     return mydir
